7def5efd5b5ac27588b7dd43cefbd88a037e869c
axel engblom <axelengblom@gmail.com>
2021-09-11 18:40:54 +0200

    added a header

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import "./App.css";
 // import Todo from "./components/Todo.js";
 import CalendarDisplay from "./components/CalendarDisplay.js";
+import Header from "./components/Header.js";
 
 function App() {
   return (
     <div className="App">
+      <Header />
       <CalendarDisplay />
     </div>
   );
---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <div className="header">
+      <h1>React Calendar With To-Do Functionalty</h1>
+    </div>
+  );
+};
+
+export default Header;
